d725d41c9c41b9cb510846ae96a28900eebc07cb
Stefano Marcato <stefano@hevelop.com>
2025-01-21 12:48:13 +0100

    Generated client for service: product-configurator with version: 1.3.0 in language: go

--- model_localisation_localized_text.go
+++ model_localisation_localized_text.go
@@ -117,26 +117,6 @@ func (o *LocalisationLocalizedText) UnmarshalJSON(data []byte) (err error) {
 	return err
 }
 
-// GetValue returns the value of well-known types
-func (o *LocalisationLocalizedText) GetValue() interface{} {
-	if o == nil || IsNil(o.AdditionalProperties) {
-		return nil
-	}
-	return o.AdditionalProperties["value"]
-}
-
-// SetValue populates the value of well-known types
-func (o *LocalisationLocalizedText) SetValue(value interface{}) {
-	if o == nil || IsNil(value) {
-		return
-	}
-	if IsNil(o.AdditionalProperties) {
-		o.AdditionalProperties = map[string]interface{}{}
-	}
-	o.AdditionalProperties["value"] = value
-	return
-}
-
 type NullableLocalisationLocalizedText struct {
 	value *LocalisationLocalizedText
 	isSet bool
